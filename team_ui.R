e0db3eb77e473781c664e6e82ddf4443d6d3bcd7
Ana De Las Alas <ajdla18@gmail.com>
2019-06-04 09:59:02 -0700

    AJ's commits. Edited Summary Page.

--- team_ui.R
+++ team_ui.R
@@ -1,44 +1,74 @@
 # Loads the needed package
 library(shiny)
 
-
-# Define UI for application that draws scatterplots
+# Define UI for application
 
 # Creates first tab of application
 tab_one <- tabPanel(
   "Overview",
-  
-  # Application title
-  titlePanel("Project Overview"),
-  
-  p("Overview."),
 
-  sidebarLayout(
-    sidebarPanel(
-    ),
-    
-    mainPanel(
-    )
+  # Application title
+  h1("An Investigation of Endangered Species"),
+  h5(em("By Ana De Las Alas, Samuel Lee, Ryan Leung and Joshua
+     Lansing")),
+
+  # Adds an image
+  img(
+    src =
+      "https://dingo.care2.com/pictures/causes/3228/3227343.large.jpg",
+    width = 800, height = 500
+  ),
+
+  # Adds paragraph with overview information
+  p("In our project, we explore a dataset on endangered species in the U.S.
+    The dataset provides information on a particular species' scientific
+    name, species group, region of habitation and critical habitat type. With
+    this dataset, we hope to answer which region of the U.S. contains the
+    most endangered and threatened species, which region contains the most
+    endangered plant species, and which species type has the greatest
+    number of acres for its critical habitat. To answer the first two
+    questions, we will focus on the columns 'regions' and 'listing status.
+    In this dataset, the U.S. Fish and Wildlife Service divides the U.S.
+    into eight regions and uses these eight regions to categorize the
+    species' habitat locations. For the last question, we will look
+    into the columns 'species group' and 'acres and miles' to compare
+    areas for critical habitats."),
+
+  # Adds an image
+  img("The photo above displays how the U.S. Fish and Wildlife Service
+      divided the country into eight regions to help identify the
+      location of different species' habitats.",
+    src = "https://www.fws.gov/where/NWRS05dotmapR9-140pixelB.jpg",
+    width = 800, height = 575
   )
-  )
+)
 
 # Creates the second tab of application
 tab_two <- tabPanel(
   "Chart One",
   titlePanel("Chart One"),
-  p("Two.")
+  p("Ryan's chart.")
 )
 
+# Creates the third tab of application
 tab_three <- tabPanel(
   "Chart Two",
   titlePanel("Chart Two"),
-  p("Three")
+  p("Sam's chart.")
 )
 
+# Creates the fourth tab of application
 tab_four <- tabPanel(
   "Chart Three",
-  titlePanel("Chart Three"),
-  p("Four"),
+  titlePanel("Comparing Acres of Critical Habitats"),
+  p("A critical habitat is a geographic area containing resources that
+    are crucial to preserve endangered and threatened species. With this
+    chart, we can compare the areas of critical habitats belonging
+    to different species groups, where the area is measured by acres. This
+    visualization helps us investigate which species group has the highest
+    average of critical habitat area. The widgets on the left allow users to
+    change the parameters of the y-axis to alter the data being shown on
+    the boxplot. The other widget changes the color of the boxes."),
   sidebarLayout(
     sidebarPanel(
       sliderInput(
@@ -50,10 +80,11 @@ tab_four <- tabPanel(
       selectInput(
         "color",
         label = "Fill Color",
-        choices = list("Turquoise" = "turquoise", "Black" = "black", "Yellow" = "yellow")
+        choices = list("Turquoise" = "turquoise",
+                       "Black" = "black", "Yellow" = "yellow")
       )
     ),
-    
+
     # Shows a plot of the generated scatterplot
     mainPanel(
       plotOutput("boxplot")
@@ -61,15 +92,38 @@ tab_four <- tabPanel(
   )
 )
 
+# Creates the fifth tab of application
 tab_five <- tabPanel(
   "Summary",
   titlePanel("Summary and Takeaways"),
-  p("Five")
+  img(
+    src =
+      "https://dbw4iivs1kce3.cloudfront.net/680x/2017/11/timflach9-11-2017.jpg"
+  ),
+  h4("Chart One: Lollipop Graph"),
+  p("Ryan's chart takeaways."),
+  h4("Chart Two: Bar Graph"),
+  p("Sam's chart takeaways."),
+  h4("Chart Three: Boxplot"),
+  p("From the boxplot, we found that when the maximum number of acres
+    displayed on the chart was 5,000 acres or more, mammals always
+    seemed to have the highest mean. This means that on average,
+    the critical habitats of mammals are larger in area than the
+    habitats of other species groups. This may be because
+    mammals overall are significantly larger in size than the
+    animals in other species groups, such as insects or
+    amphibians. We also noticed how the whiskers of the box for
+    mammals typically were longer than the whiskers of other boxes.
+    This indicates that the interquartile range for mammals'
+    critical habitat area is much larger than that for other species
+    groups. We can infer that critical habitats for mammals that
+    exist in the U.S. are more diverse in size, and can either be
+    substantially small or large.")
 )
 
 # Combines the tabs
 shinyUI <- navbarPage(
-  "Endangered Animals",
+  "Endangered Species",
   tab_one,
   tab_two,
   tab_three,
